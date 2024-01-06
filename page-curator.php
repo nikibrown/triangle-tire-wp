7ee5c5b73481277e74440ea9d89e4c8d18fc0858
Niki Brown <hi@nikibrown.com>
2024-01-06 09:08:24 -0800

    remove modal, move form onto page

--- page-curator.php
+++ page-curator.php
@@ -35,133 +35,121 @@ get_header();
 <div class="interiorbanner expanded row">
     <div class="large-12">
         <?php
-                    if( !empty( $pics ) )
-                    {
-                        echo '<div class="js-gallery" style="height: 300px; max-height: 300px; overflow: hidden;">';
-                        foreach( $pics as $g )
-                        {
-                            echo '<img src="'. $g .'" />';
-                        }
-                        echo '</div>';
-                    }
-                    else
-                    {
-                        echo '<img src="'.$img.'" />';
-                    }
-                    ?>
+            if( !empty( $pics ) )
+            {
+                echo '<div class="js-gallery" style="height: 300px; max-height: 300px; overflow: hidden;">';
+                foreach( $pics as $g )
+                {
+                    echo '<img src="'. $g .'" />';
+                }
+                echo '</div>';
+            }
+            else
+            {
+                echo '<img src="'.$img.'" />';
+            }
+            ?>
     </div>
 </div>
 <?php
         }
         ?>
-<div id="gearheadswrap" class="container-fluid curator">
+<div class="container-fluid curator">
     <div id="gearheads" class="row">
         <div class="medium-12 large-12 columns gearheadcontent">
             <div id="gearheadsinner" class="row scoopcolumns">
                 <div class="large-12">
                     <div class="">
-                        <section class="semantic-content reveal" id="modal-register" tabindex="-1" role="dialog"
-                            aria-labelledby="modal-register" aria-hidden="true" data-reveal>
-                            <div class="modal-inner">
-                                <div class="modal-content">
-                                    <a class="close-button" data-close aria-label="Close modal" type="button"><span
-                                            aria-hidden="true">&times;</span>
-                                    </a>
-                                    <div id="mc_embed_shell">
-                                        <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css"
-                                            rel="stylesheet" type="text/css">
-                                        <style type="text/css">
-                                        #mc_embed_signup {
-                                            background: #fff;
-                                            false;
-                                            clear: left;
-                                            font: 14px Helvetica, Arial, sans-serif;
-                                            width: 600px;
-                                        }
-                                        </style>
-                                        <div id="mc_embed_signup">
-                                            <form
-                                                action="https://triangletireus.us20.list-manage.com/subscribe/post?u=234d050d07c5717cb90727df8&amp;id=f4093a4aa7&amp;f_id=008c3aeaf0"
-                                                method="post" id="mc-embedded-subscribe-form"
-                                                name="mc-embedded-subscribe-form" class="validate" target="_blank">
-                                                <div id="mc_embed_signup_scroll">
-                                                    <h3>Register</h3>
-                                                    <div class="indicates-required"><span class="asterisk">*</span>
-                                                        indicates required</div>
-                                                    <div class="mc-field-group"><label for="mce-FNAME">First Name
-                                                        </label><input type="text" name="FNAME" class=" text"
-                                                            id="mce-FNAME" value=""></div>
-                                                    <div class="mc-field-group"><label for="mce-LNAME">Last Name
-                                                        </label><input type="text" name="LNAME" class=" text"
-                                                            id="mce-LNAME" value=""></div>
-                                                    <div class="mc-field-group"><label for="mce-EMAIL">Email Address
-                                                            <span class="asterisk">*</span></label><input type="email"
-                                                            name="EMAIL" class="required email" id="mce-EMAIL" value=""
-                                                            required=""><span id="mce-EMAIL-HELPERTEXT"
-                                                            class="helper_text"></span></div>
-                                                    <div id="mce-responses" class="clear">
-                                                        <div class="response" id="mce-error-response"
-                                                            style="display: none;"></div>
-                                                        <div class="response" id="mce-success-response"
-                                                            style="display: none;"></div>
-                                                    </div>
-                                                    <div aria-hidden="true" style="position: absolute; left: -5000px;">
-                                                        <input type="text" name="b_234d050d07c5717cb90727df8_f4093a4aa7"
-                                                            tabindex="-1" value="">
-                                                    </div>
-                                                    <div class="clear"><input type="submit" name="subscribe"
-                                                            id="mc-embedded-subscribe" class="button" value="Subscribe">
-                                                    </div>
-                                                </div>
-                                            </form>
-                                        </div>
-                                        <script type="text/javascript"
-                                            src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
-                                        <script type="text/javascript">
-                                        (function($) {
-                                            window.fnames = new Array();
-                                            window.ftypes = new Array();
-                                            fnames[1] = 'FNAME';
-                                            ftypes[1] = 'text';
-                                            fnames[2] = 'LNAME';
-                                            ftypes[2] = 'text';
-                                            fnames[0] = 'EMAIL';
-                                            ftypes[0] = 'email';
-                                            fnames[3] = 'ADDRESS';
-                                            ftypes[3] = 'address';
-                                            fnames[4] = 'PHONE';
-                                            ftypes[4] = 'phone';
-                                            fnames[5] = 'BIRTHDAY';
-                                            ftypes[5] = 'birthday';
-                                        }(jQuery));
-                                        var $mcj = jQuery.noConflict(true);
-                                        </script>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </section>
                         <div class="row js-second-alert">
                             <div class="small-12 columns">
-                                <div class="callout callout-instruction dark">
-                                    <p class="text-center">
-                                        Register here to receive weekly highlight emails on top OTR news.<br><a
-                                            href="#modal-register" class="call-modal" title=""
-                                            data-open="modal-register" aria-controls="modal-register"
-                                            aria-haspopup="true" tabindex="0"><button class="button specialbutton"
-                                                type="button">Register</button></a>
-                                    </p>
+                                <div id="mc_embed_shell">
+                                    <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css"
+                                        rel="stylesheet" type="text/css">
+                                    <style type="text/css">
+                                    #mc_embed_signup {
+                                        background: #fff;
+                                        false;
+                                        clear: left;
+                                        font: 14px Helvetica, Arial, sans-serif;
+                                        width: 600px;
+                                    }
+                                    </style>
+                                    <div id="mc_embed_signup">
+                                        <form
+                                            action="https://triangletireus.us20.list-manage.com/subscribe/post?u=234d050d07c5717cb90727df8&amp;id=f4093a4aa7&amp;f_id=008c3aeaf0"
+                                            method="post" id="mc-embedded-subscribe-form"
+                                            name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                                            <div id="mc_embed_signup_scroll">
+                                                <h1>Register here to receive weekly highlight emails on top OTR news.
+                                                </h1>
+                                                <div class="indicates-required"><span class="asterisk">*</span>
+                                                    indicates required</div>
+                                                <div class="mc-field-group"><label for="mce-FNAME">First Name
+                                                    </label><input type="text" name="FNAME" class=" text" id="mce-FNAME"
+                                                        value=""></div>
+                                                <div class="mc-field-group"><label for="mce-LNAME">Last Name
+                                                    </label><input type="text" name="LNAME" class=" text" id="mce-LNAME"
+                                                        value=""></div>
+                                                <div class="mc-field-group"><label for="mce-EMAIL">Email Address
+                                                        <span class="asterisk">*</span></label><input type="email"
+                                                        name="EMAIL" class="required email" id="mce-EMAIL" value=""
+                                                        required=""><span id="mce-EMAIL-HELPERTEXT"
+                                                        class="helper_text"></span></div>
+                                                <div id="mce-responses" class="clear">
+                                                    <div class="response" id="mce-error-response"
+                                                        style="display: none;"></div>
+                                                    <div class="response" id="mce-success-response"
+                                                        style="display: none;"></div>
+                                                </div>
+                                                <div aria-hidden="true" style="position: absolute; left: -5000px;">
+                                                    <input type="text" name="b_234d050d07c5717cb90727df8_f4093a4aa7"
+                                                        tabindex="-1" value="">
+                                                </div>
+                                                <div class="clear"><input type="submit" name="subscribe"
+                                                        id="mc-embedded-subscribe" class="button" value="Subscribe">
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <script type="text/javascript"
+                                        src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+                                    <script type="text/javascript">
+                                    (function($) {
+                                        window.fnames = new Array();
+                                        window.ftypes = new Array();
+                                        fnames[1] = 'FNAME';
+                                        ftypes[1] = 'text';
+                                        fnames[2] = 'LNAME';
+                                        ftypes[2] = 'text';
+                                        fnames[0] = 'EMAIL';
+                                        ftypes[0] = 'email';
+                                        fnames[3] = 'ADDRESS';
+                                        ftypes[3] = 'address';
+                                        fnames[4] = 'PHONE';
+                                        ftypes[4] = 'phone';
+                                        fnames[5] = 'BIRTHDAY';
+                                        ftypes[5] = 'birthday';
+                                    }(jQuery));
+                                    var $mcj = jQuery.noConflict(true);
+                                    </script>
                                 </div>
                             </div>
                         </div>
-
-                        <?php the_content(); ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<div id="gearheadswrap" class="container-fluid curator">
+    <div id="gearheads" class="row">
+        <div class="medium-12 large-12 columns gearheadcontent">
+            <?php the_content(); ?>
+        </div>
+    </div>
+</div>
+
 <script src="<?php bloginfo( 'template_url' ); ?>/js/vendor/foundation.reveal.js"></script>
 <?php
 
