65052a88a23ff2e7a2545509b2aa9e0313f8aa0c
Niki Brown <hi@nikibrown.com>
2024-01-06 14:51:51 -0800

    update header bloginfo links

--- header.php
+++ header.php
@@ -14,6 +14,7 @@ if( $favicon == '' )
 ?>
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -31,21 +32,22 @@ if( $favicon == '' )
     <script src="https://use.fontawesome.com/e83b344081.js"></script>
 
     <script type="text/javascript">
-        var site_options = {
-            base: '<?php echo home_url(); ?>'
-        };
+    var site_options = {
+        base: '<?php echo home_url(); ?>'
+    };
     </script>
     <?php wp_head(); ?>
 </head>
-<body <?php body_class('class-name');?> >
-<div id="triangle-overlay">
-	<img src="<?php bloginfo( 'template_url' ); ?>/images/Triangle _spattern_overlay.png">
-</div>
-<div id="topheaderrow" class="hideonmobile expanded row">
-	<div class="large-12 columns">
-		<div class="row">
-			<div class="large-12 columns">
-				<?php
+
+<body <?php body_class('class-name');?>>
+    <div id="triangle-overlay">
+        <img src="<?php bloginfo( 'template_url' ); ?>/images/Triangle _spattern_overlay.png">
+    </div>
+    <div id="topheaderrow" class="hideonmobile expanded row">
+        <div class="large-12 columns">
+            <div class="row">
+                <div class="large-12 columns">
+                    <?php
             wp_nav_menu( 
                     array(
                     'menu' => 'Primary Menu',
@@ -56,7 +58,7 @@ if( $favicon == '' )
                 )
             );
             ?>
-				<!--<ul class="dropdown menu secondary" data-dropdown-menu>
+                    <!--<ul class="dropdown menu secondary" data-dropdown-menu>
 					<li><a href="<?php bloginfo( 'home' ); ?>/about">About Us</a></li>
 					<li><a href="<?php bloginfo( 'home' ); ?>/support-center">Support Center</a></li>
 					<li><a href="<?php bloginfo( 'home' ); ?>/contact">Contact Us</a></li>
@@ -65,39 +67,45 @@ if( $favicon == '' )
 					<li><?php pll_the_languages(array('show_flags' => 1, 'hide_current' => 1 )); ?></li>
 				</ul>-->
 
-				<section class="semantic-content reveal" id="modal-text" tabindex="-1" role="dialog" aria-labelledby="modal-label" aria-hidden="true" data-reveal>
-					<div class="modal-inner">
-						<div class="modal-content">
-							<a class="close-button" data-close aria-label="Close modal" type="button">
-  								<span aria-hidden="true">&times;</span>
-							</a>
-							<form class="form-inline" action="<?php bloginfo('home'); ?>/" method="get">
-								<label>Search by Tire Size or Free Text for: </label>
-								<input class="form-control js-autocomplete-search" type="text" name="s" value="<?php if (!get_search_query()) : echo ''; else : echo get_search_query(); endif; ?>" onfocus="if (this.value == 'search') {this.value = '';}" onblur="if (this.value == '') {this.value = 'search';}" />
-								<input type="submit" class="btn btn-default" value="Search" />
-							</form>
-						</div>
-					</div>
-				</section>
+                    <section class="semantic-content reveal" id="modal-text" tabindex="-1" role="dialog"
+                        aria-labelledby="modal-label" aria-hidden="true" data-reveal>
+                        <div class="modal-inner">
+                            <div class="modal-content">
+                                <a class="close-button" data-close aria-label="Close modal" type="button">
+                                    <span aria-hidden="true">&times;</span>
+                                </a>
+                                <form class="form-inline" action="<?php bloginfo('url'); ?>/" method="get">
+                                    <label>Search by Tire Size or Free Text for: </label>
+                                    <input class="form-control js-autocomplete-search" type="text" name="s"
+                                        value="<?php if (!get_search_query()) : echo ''; else : echo get_search_query(); endif; ?>"
+                                        onfocus="if (this.value == 'search') {this.value = '';}"
+                                        onblur="if (this.value == '') {this.value = 'search';}" />
+                                    <input type="submit" class="btn btn-default" value="Search" />
+                                </form>
+                            </div>
+                        </div>
+                    </section>
 
-			</div>
-		</div>
-	</div>
-</div>
-<div id="header" class="expanded row">
-    <div class="large-12 no-print">
-        <div id="mainheaderrow" class="row">
-            <div class="hideonmobile medium-12 large-3 columns">
-				<a class="logolink" href="<?php bloginfo( 'home' ); ?>"><img class="logo" src="<?php bloginfo( 'template_url' ); ?>/images/Triangle_logo_tag-WHT.png"></a>
-            </div>
-            <div class="medium-12 large-9 columns">
-				<a class="hideondesktop logolink" href="<?php bloginfo( 'home' ); ?>"><img class="logo" src="<?php bloginfo( 'template_url' ); ?>/images/Triangle_logo_tag-WHT.png"></a>
-                <div class="title-bar" data-responsive-toggle="example-animated-menu" data-hide-for="large">
-                    <button class="menu-icon" type="button" data-toggle></button>
                 </div>
-                <div class="top-bar" id="example-animated-menu" data-equalizer>
-                    <div class="top-bar-left" data-equalizer-watch>
-                    		<?php
+            </div>
+        </div>
+    </div>
+    <div id="header" class="expanded row">
+        <div class="large-12 no-print">
+            <div id="mainheaderrow" class="row">
+                <div class="hideonmobile medium-12 large-3 columns">
+                    <a class="logolink" href="<?php bloginfo( 'url' ); ?>"><img class="logo"
+                            src="<?php bloginfo( 'template_url' ); ?>/images/Triangle_logo_tag-WHT.png"></a>
+                </div>
+                <div class="medium-12 large-9 columns">
+                    <a class="hideondesktop logolink" href="<?php bloginfo( 'url' ); ?>"><img class="logo"
+                            src="<?php bloginfo( 'template_url' ); ?>/images/Triangle_logo_tag-WHT.png"></a>
+                    <div class="title-bar" data-responsive-toggle="example-animated-menu" data-hide-for="large">
+                        <button class="menu-icon" type="button" data-toggle></button>
+                    </div>
+                    <div class="top-bar" id="example-animated-menu" data-equalizer>
+                        <div class="top-bar-left" data-equalizer-watch>
+                            <?php
 								wp_nav_menu( 
 										  array(
 										  'menu' => 'Tire Menu',
@@ -108,15 +116,15 @@ if( $favicon == '' )
 									 )
 								);
 								?>
-								<!--<a href="#modal-text" class="searchbutton call-modal" title="Search" data-open="modal-text">Search</a>-->
-								
-                        <!--<ul class="dropdown menu main" data-dropdown-menu>
+                            <!--<a href="#modal-text" class="searchbutton call-modal" title="Search" data-open="modal-text">Search</a>-->
+
+                            <!--<ul class="dropdown menu main" data-dropdown-menu>
                             <li><a href="<?php bloginfo( 'home' ); ?>/passenger-tires">Passenger / Light Truck / SUV Tires</a></li>
                             <li><a href="<?php bloginfo( 'home' ); ?>/truck-tires">Truck &amp; Bus Tires</a></li>
                             <li><a href="<?php bloginfo( 'home' ); ?>/offroad-tires">Off-The-Road Tires</a></li>
                         </ul>-->
-                        
-                        <?php
+
+                            <?php
 								wp_nav_menu( 
 										  array(
 										  'menu' => 'Primary Menu',
@@ -127,8 +135,8 @@ if( $favicon == '' )
 									 )
 								);
 								?>
-								
-								<!--<ul class="hideondesktop dropdown menu secondary" data-dropdown-menu>
+
+                            <!--<ul class="hideondesktop dropdown menu secondary" data-dropdown-menu>
 									<li><a href="<?php bloginfo( 'home' ); ?>/about">About Us</a></li>
 									<li><a href="<?php bloginfo( 'home' ); ?>/support-center">Support Center</a></li>
 									<li><a href="<?php bloginfo( 'home' ); ?>/contact">Contact Us</a></li>
@@ -136,29 +144,35 @@ if( $favicon == '' )
 									<li><a href="#modal-text" class="searchbutton call-modal" title="Search" data-open="modal-text">Search</a></li>
 								</ul>
 								<a class="hideondesktop" href="#modal-text" class="searchbutton call-modal" title="Search" data-open="modal-text">Search</a>-->
-								<ul class="mobilesocial">
-									<li>
-									<a href="https://business.facebook.com/TriangleTireUSA" target="_blank"><span class="fab fa-facebook"></span>  &nbsp; </a>
-									</li>
-									<li>
-									<a href="http://twitter.com/TriangleTireUSA"><span class="fab fa-twitter"></span>  &nbsp; </a>
-									</li>
-									<li>
-									<a href="https://www.instagram.com/triangletireusa"><span class="fab fa-instagram"></span>  &nbsp; </a>
-									</li>
-									<li>
-									<a href="https://www.linkedin.com/company/triangle-tires-usa" target="_blank"><span class="fab fa-linkedin-in"></span>  &nbsp; </a>
-									</li>
-									<li>
-									<a href="https://www.youtube.com/channel/UCLt9WPyKi39SWFDeA8tceyQ/about"><span class="fab fa-youtube"></span>  &nbsp; </a>
-									</li>
-									<li>
-									<a href="http://tt.devbuilds.site/find-a-dealer"><span class="fas fa-map-marker-alt"></span>  &nbsp; </a>
-									</li>
-								</ul>
+                            <ul class="mobilesocial">
+                                <li>
+                                    <a href="https://business.facebook.com/TriangleTireUSA" target="_blank"><span
+                                            class="fab fa-facebook"></span> &nbsp; </a>
+                                </li>
+                                <li>
+                                    <a href="http://twitter.com/TriangleTireUSA"><span class="fab fa-twitter"></span>
+                                        &nbsp; </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/triangletireusa"><span
+                                            class="fab fa-instagram"></span> &nbsp; </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/company/triangle-tires-usa" target="_blank"><span
+                                            class="fab fa-linkedin-in"></span> &nbsp; </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.youtube.com/channel/UCLt9WPyKi39SWFDeA8tceyQ/about"><span
+                                            class="fab fa-youtube"></span> &nbsp; </a>
+                                </li>
+                                <li>
+                                    <a href="http://tt.devbuilds.site/find-a-dealer"><span
+                                            class="fas fa-map-marker-alt"></span> &nbsp; </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
